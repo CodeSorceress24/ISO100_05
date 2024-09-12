ed6e9d29da950dc5659c0ffaa71520bc1c448d93
Marcela <marcela.maciel2408@gmail.com>
2024-09-11 22:25:22 -0300

    Add files via upload

---
+++ ISO100_05/Ex01/controller/Threads.java
@@ -0,0 +1,71 @@
+package controller;
+import java.util.concurrent.Semaphore;
+import java.util.Random;
+
+public class Threads extends Thread {
+
+    private int idBancarrio;
+    private Semaphore semaforo;
+
+    public ClasseThreads(int idBancarrio, Semaphore semaforo){
+        this.idBancarrio = idBancarrio;
+        this.semaforo = semaforo;
+    }
+
+    public void calc(int resto, int idBancarrio, int indx) {
+        Random rad = new Random();
+        int t;
+        if (resto == 1){
+            t = rad.nextInt(800) + 200;
+        } else if (resto == 2) {
+            t = rad.nextInt(1000) + 500;
+        } else { // resto == 0
+            t = rad.nextInt(1000) + 1000;
+        }
+        try {
+            sleep(t);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("#" + idBancarrio + ":" + (indx + 1) + "º cálculo concluído!");
+        }
+    }
+
+    public void tran(int resto, int idBancarrio, int indx) {
+        int t;
+        if (resto == 1){
+            t = 1000;
+        } else { 
+            t = 1500;
+        }
+        try {
+            sleep(t);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("#" + idBancarrio + ": " + (indx + 1) + "º transação concluída!");
+        }
+    }
+
+    @Override
+    public void run(){
+        int resto = idBancarrio % 3;
+        int loop = 0;
+        if (resto == 1){
+            loop = 2;
+        } else {
+            loop = 3;
+        }
+        for (int i = 0; i < loop; i++) {
+            calc(resto, idBancarrio, i);
+            try {
+                semaforo.acquire();
+                tran(resto, idBancarrio, i);
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                semaforo.release();
+            }
+        }
+    }
+}
